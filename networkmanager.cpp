5653ae06e58daef151cb291de2d851bb7ddf9239
minsikoh <41605844+minsikoh@users.noreply.github.com>
2024-05-14 09:25:08 +0900

    Update networkmanager.cpp

--- networkmanager.cpp
+++ networkmanager.cpp
@@ -61,9 +61,7 @@ void CNetworkManager::RecvCallback(char* data, int orgSize)
 		printf("failed checksum %d : %d\n", (short)rcvMsg.MSG_CKS, ckSum);
 	}
 	
-	
-		
-	printf("HDR1(%d)HDR2(%d)CNT(%d)AST1(%d)AST2(%d)AST3(%d)AST4(%d)UTC1(%d)UTC2(%d)UTC3(%d)UTC4(%d)UTC5(%d)TAIL(%d)CKS(%d)\n", rcvMsg.HDR_1, rcvMsg.HDR_2, rcvMsg.CNT, rcvMsg.AST_1,rcvMsg.AST_2, rcvMsg.AST_3, rcvMsg.AST_4, rcvMsg.UTC_DATA_1, rcvMsg.UTC_DATA_2, rcvMsg.UTC_DATA_3, rcvMsg.UTC_DATA_4, rcvMsg.UTC_DATA_5, rcvMsg.TAIL, (short)rcvMsg.MSG_CKS );
+
 
 }
 
@@ -71,7 +69,6 @@ void CNetworkManager::SendCallback(char* data, int orgSize)
 {
 	CNetworkManager::getInstance().CreateVirtualData();
 
-//	printf("S-> HDR1(%d) HDR2(%d) CNT(%d)\n", CNetworkManager::getInstance().sendMsg.HDR_1, CNetworkManager::getInstance().sendMsg.HDR_2, CNetworkManager::getInstance().sendMsg.CNT);
 	memcpy(data, &CNetworkManager::getInstance().sendMsg, sizeof(RS422_MSG));
 }
 
@@ -103,17 +100,5 @@ short CNetworkManager::udp_checksum(char* message, int nWords)
 
 void CNetworkManager::CreateVirtualData()
 {
-	sendMsg.HDR_1 = 3;
-	sendMsg.HDR_2 = 4;
-	sendMsg.CNT = (sendMsg.CNT + 1) % 255;
-
-	sendMsg.AST_1 = 32;
-	sendMsg.AST_2 = 5;
-	sendMsg.AST_3 = 9;
-	sendMsg.AST_4 = 52;
-
-	sendMsg.TAIL = 0x7D;
-
-	sendMsg.MSG_CKS = udp_checksum((char*)&sendMsg, sizeof(RS422_MSG)/2 - 2);
 
 }
